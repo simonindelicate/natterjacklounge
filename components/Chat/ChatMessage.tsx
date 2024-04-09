9109cb736f3fce0ff2c4b800ac6627af728b69ec
simonindelicate <78307744+simonindelicate@users.noreply.github.com>
2024-04-09 13:40:40 +0100

    added recognition logic

--- components/Chat/ChatMessage.tsx
+++ components/Chat/ChatMessage.tsx
@@ -85,9 +85,9 @@ export const ChatMessage: FC<Props> = ({ message, containsGingerbread }) => {
     let linkedContent = content;
 
 cocktailNames.forEach((name) => {
-  const pageLink = getPageLink(name);
-  const regexPattern = `\"?${name}\"?[,.!?]?`;
-  const regex = new RegExp(name, 'gi');
+  const pageLink = getPageLink(name.toLowerCase());
+  const regex = new RegExp('\\b' + name.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&') + '\\b', 'gi');
+   console.log(`Linking: ${name}, Regex: ${regex}, Link: ${pageLink}`); // Debug line
   linkedContent = linkedContent.replace(regex, (match) => `<a href="${pageLink}" onclick="event.preventDefault(); window.open('${pageLink}', 'popup', 'width=600,height=600,left=50,top=50');">${match}<svg style="display: inline-block; vertical-align: top; font-size:50%; padding-right:5px;" xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M320 0c-17.7 0-32 14.3-32 32s14.3 32 32 32h82.7L201.4 265.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L448 109.3V192c0 17.7 14.3 32 32 32s32-14.3 32-32V32c0-17.7-14.3-32-32-32H320zM80 32C35.8 32 0 67.8 0 112V432c0 44.2 35.8 80 80 80H400c44.2 0 80-35.8 80-80V320c0-17.7-14.3-32-32-32s-32 14.3-32 32V432c0 8.8-7.2 16-16 16H80c-8.8 0-16-7.2-16-16V112c0-8.8 7.2-16 16-16H192c17.7 0 32-14.3 32-32s-14.3-32-32-32H80z"/></svg></a>`
   );
 });
